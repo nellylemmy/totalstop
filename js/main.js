1568d043f300f526ab7adbf6525ffa4e36f6f795
nelson <lemein>
2020-09-24 16:31:34 +0300

    converting images to 2rows on mobile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // Any javascript code will go here
+
+let windowWidth = $(document).width();
+if(windowWidth <= 992){
+    document.querySelector(".founders").classList.remove("three");
+    document.querySelector(".founders").classList.add("two");
+} else{
+    document.querySelector(".founders").classList.add("three");
+}
